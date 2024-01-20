47cff8f9367c4aed5282e0b8d7b4244a29234a44
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-01-20 14:38:01 +0700

    Add route (home,sign-in,sign-up,about-us)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './componet/Home.jsx'
+import SignIn from "./componet/SignIn.jsx";
+import SignUp from "./componet/Signup.jsx";
+import AboutUs from "./componet/AboutUs.jsx";
 function App() {
 
   return (
-  <></>
+<BrowserRouter>
+<Routes>
+<Route path ="/" element = {<Home/>} />
+<Route path ="/sign-in" element = {<SignIn/>} />
+<Route path ="/sign-up" element = {<SignUp/>} />
+<Route path ="/about-us" element = {<AboutUs/>} />
+</Routes>
+</BrowserRouter>
   );
 }
 
